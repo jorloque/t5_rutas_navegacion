8480478f1de2545ac20fbed4278a37a67a7476fa
Jorge Lopez <jorloque@gmail.com>
2025-10-20 23:23:37 +0200

    Inicio del proyecto

---
+++ app/src/main/java/com/lopez_jorge/rutas_temas/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.lopez_jorge.rutas_temas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lopez_jorge.rutas_temas.ui.theme.Rutas_temasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Rutas_temasTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(start = 30.dp, end = 20.dp, top=50.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
